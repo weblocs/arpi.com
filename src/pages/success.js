cf52bedb4f02a97c2e8a5de984033f94f6a45abf
maciekkociela <kocielam@gmail.com>
2019-05-05 13:40:15 +0200

    Success page

---
+++ src/pages/success.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { graphql, StaticQuery, Link } from "gatsby";
+
+import Container from "../components/Container";
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query allSuccessPageData {
+            allWordpressPage(sort: { fields: menu_order, order: ASC }) {
+              edges {
+                node {
+                  title
+                  slug
+                  acf {
+                    button
+                    link
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={data => (
+          <div className="mainSection">
+            <Container>
+              <p>Success</p>
+              <Link to={data.allWordpressPage.edges[0].node.acf.link}>
+                {data.allWordpressPage.edges[0].node.acf.button}
+              </Link>
+            </Container>
+
+            <style jsx>{`
+              .mainSection {
+                height: 100vh;
+                display: flex;
+                align-items: center;
+              }
+            `}</style>
+          </div>
+        )}
+      />
+    );
+  }
+}
+
+export default IndexPage;
