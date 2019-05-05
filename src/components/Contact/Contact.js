e89e53192a8924fa30c36216f2575c169c8d164b
maciekkociela <kocielam@gmail.com>
2019-05-05 12:22:44 +0200

    Test netlify contact forms

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -58,11 +58,30 @@ const Contact = props => {
       <ThemeContext.Consumer>
         {theme => (
           <div className="form">
-            <Form
+          <form name="test" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+            {/* <Form
               name="contact"
               onSubmit={handleSubmit}
               data-netlify="true"
-              data-netlify-honeypot="bot-field"
             >
               <FormItem label="Name">
                 {getFieldDecorator("name", {
@@ -99,7 +118,7 @@ const Contact = props => {
                   Submit
                 </Button>
               </FormItem>
-            </Form>
+            </Form> */}
 
             {/* --- STYLES --- */}
             <style jsx>{`
